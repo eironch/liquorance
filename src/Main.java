7e0f367eb9973056f7e3127aa572927eaa42fdfd
ficuno <eldritchgallows@gmail.com>
2024-01-07 21:27:09 +0800

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import pages.LandingPage;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            new LandingPage();
+        });
     }
 }
