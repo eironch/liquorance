014c3abe3b712047b640f97ec1d15ee295b59470
ficuno <eldritchgallows@gmail.com>
2024-01-07 21:28:04 +0800

    update code

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new LandingPage();
-        });
+        SwingUtilities.invokeLater(LandingPage::new);
     }
 }
