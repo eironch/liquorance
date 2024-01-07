780151c612ea0b4be7525dd5bd2e7f2e4f2bdd35
ficuno <eldritchgallows@gmail.com>
2024-01-07 22:28:21 +0800

    added class

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import pages.LandingPage;
-
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(LandingPage::new);
-    }
-}
