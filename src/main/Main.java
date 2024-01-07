1a4226f196067c73e14d6dc69d6f1f7a60a9feb3
ficuno <eldritchgallows@gmail.com>
2024-01-07 22:28:52 +0800

    updated code

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import asset.AssetFactory;
+import component.ComponentFactory;
+import container.PanelFactory;
+import page.LandingPage;
+
+import javax.swing.*;
+
+public class Main {
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 800;
+    final static PanelFactory panelFactory = new PanelFactory();
+    final static ComponentFactory componentFactory = new ComponentFactory();
+    final static AssetFactory assetFactory = new AssetFactory();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LandingPage(panelFactory, componentFactory, assetFactory));
+    }
+}
