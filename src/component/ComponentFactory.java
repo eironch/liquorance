bd966f8c6905ffaa107fe24d3a1cdf6aa7e26151
ficuno <eldritchgallows@gmail.com>
2024-01-08 22:38:32 +0800

    update code

--- src/component/ComponentFactory.java
+++ src/component/ComponentFactory.java
@@ -1,5 +1,26 @@
 package component;
 
-public class ComponentFactory {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
+public class ComponentFactory {
+    public JLabel titleText = new JLabel();
+    public ArrayList<JButton> catalogButtons = new ArrayList<>();
+    public ComponentFactory() {
+        titleText.setText("Liquorance");
+        titleText.setFont(toHelvetica(40));
+        titleText.setForeground(Color.WHITE);
+
+        for (int i = 0; i < 8; i++) {
+            catalogButtons.add(new JButton());
+            catalogButtons.get(i).setText("Liquor" + i);
+            catalogButtons.get(i).setFocusable(false);
+            catalogButtons.get(i).setPreferredSize(new Dimension(50, 50));
+        }
+    }
+
+    public Font toHelvetica(int size){
+        return new Font("Helvetica", Font.BOLD, size);
+    }
 }
