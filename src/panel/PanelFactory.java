5ab5895cc8f2d747cca6e132e1f2889696e72956
ficuno <eldritchgallows@gmail.com>
2024-01-08 23:04:53 +0800

    add catalog containers

--- src/panel/PanelFactory.java
+++ src/panel/PanelFactory.java
@@ -3,17 +3,39 @@ package panel;
 import main.Main;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PanelFactory {
     public JPanel content = new JPanel();
-    public JPanel headerContainer = new JPanel();
+    public JPanel headerPanel = new JPanel();
+    public JPanel bodyPanel = new JPanel();
+    public JPanel footerPanel = new JPanel();
+    public ArrayList<Container> catalogRowContainerList = new ArrayList<>();
+    public ArrayList<Container> catalogContainerList = new ArrayList<>();
 
     public PanelFactory() {
         content.setLayout(new BorderLayout(0, 0));
-        content.setBackground(Color.LIGHT_GRAY);
 
-        headerContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
-        headerContainer.setPreferredSize(new Dimension(Main.WIDTH, 80));
-        headerContainer.setBackground(Color.DARK_GRAY);
+        headerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
+        headerPanel.setPreferredSize(new Dimension(Main.WIDTH, 80));
+        headerPanel.setBackground(Color.DARK_GRAY);
+
+        bodyPanel.setLayout(new GridLayout(2, 1, 0,0));
+        bodyPanel.setPreferredSize(new Dimension(Main.WIDTH, 200));
+        bodyPanel.setBackground(Color.LIGHT_GRAY);
+
+        footerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
+        footerPanel.setPreferredSize(new Dimension(Main.WIDTH, 80));
+        footerPanel.setBackground(Color.DARK_GRAY);
+
+        for (int i = 0; i < 2; i++) {
+            catalogRowContainerList.add(new Container());
+            catalogRowContainerList.get(i).setLayout(new GridLayout(1, 4, 0, 0));
+        }
+
+        for (int i = 0; i < 8; i++) {
+            catalogContainerList.add(new Container());
+            catalogContainerList.get(i).setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        }
     }
 }
