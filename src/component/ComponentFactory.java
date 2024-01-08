e08e423b0e262e5ffeefdbb7d5f97a3b6e042656
ficuno <eldritchgallows@gmail.com>
2024-01-08 23:04:26 +0800

    add catalog buttons

--- src/component/ComponentFactory.java
+++ src/component/ComponentFactory.java
@@ -6,17 +6,18 @@ import java.util.ArrayList;
 
 public class ComponentFactory {
     public JLabel titleText = new JLabel();
-    public ArrayList<JButton> catalogButtons = new ArrayList<>();
+    public ArrayList<JButton> catalogButtonList = new ArrayList<>();
+
     public ComponentFactory() {
         titleText.setText("Liquorance");
         titleText.setFont(toHelvetica(40));
         titleText.setForeground(Color.WHITE);
 
         for (int i = 0; i < 8; i++) {
-            catalogButtons.add(new JButton());
-            catalogButtons.get(i).setText("Liquor" + i);
-            catalogButtons.get(i).setFocusable(false);
-            catalogButtons.get(i).setPreferredSize(new Dimension(50, 50));
+            catalogButtonList.add(new JButton());
+            catalogButtonList.get(i).setText("Liquor " + i);
+            catalogButtonList.get(i).setPreferredSize(new Dimension(320, 200));
+            catalogButtonList.get(i).setFocusable(false);
         }
     }
 
