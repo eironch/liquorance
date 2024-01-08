4ef3fc15fd354ee08bad99bc399e0e23fcf2f896
ficuno <eldritchgallows@gmail.com>
2024-01-09 00:03:49 +0800

    add database

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,24 @@
 package main;
 
+import database.DatabaseManager;
 import asset.AssetFactory;
 import component.ComponentFactory;
 import panel.PanelFactory;
 import page.LandingPage;
-
 import javax.swing.*;
 
 public class Main {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 800;
+    final static DatabaseManager databaseManager = new DatabaseManager();
     final static PanelFactory panelFactory = new PanelFactory();
     final static ComponentFactory componentFactory = new ComponentFactory();
     final static AssetFactory assetFactory = new AssetFactory();
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LandingPage(panelFactory, componentFactory, assetFactory));
+        SwingUtilities.invokeLater(() -> new LandingPage(
+                databaseManager, panelFactory, componentFactory,
+                assetFactory).setVisible(true)
+        );
     }
 }
