bb077ad10cf63dde013e072db442d7643741281e
ficuno <eldritchgallows@gmail.com>
2024-01-09 17:56:48 +0800

    add category and header components

--- src/component/ComponentFactory.java
+++ src/component/ComponentFactory.java
@@ -6,13 +6,24 @@ import java.util.ArrayList;
 
 public class ComponentFactory {
     public JLabel titleText = new JLabel();
+    public JButton changeViewButton = new JButton();
+    public JButton orderButton = new JButton();
     public ArrayList<JButton> catalogButtonList = new ArrayList<>();
     public ArrayList<JLabel> catalogNameList = new ArrayList<>();
+    public ArrayList<JButton> categoryButtonList = new ArrayList<>();
 
     public ComponentFactory() {
         titleText.setText("Liquorance");
         titleText.setFont(toHelvetica(40));
         titleText.setForeground(Color.WHITE);
+
+        changeViewButton.setText("View");
+        changeViewButton.setPreferredSize(new Dimension(100,60));
+        changeViewButton.setFocusable(false);
+
+        orderButton.setText("Order");
+        orderButton.setPreferredSize(new Dimension(200,60));
+        orderButton.setFocusable(false);
 
         for (int i = 0; i < 8; i++) {
             catalogButtonList.add(new JButton());
@@ -27,6 +38,13 @@ public class ComponentFactory {
             catalogNameList.get(i).setFont(toHelvetica(15));
             catalogNameList.get(i).setPreferredSize(new Dimension(320, 60));
             catalogNameList.get(i).setHorizontalAlignment(JLabel.CENTER);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            categoryButtonList.add(new JButton());
+            categoryButtonList.get(i).setText("" + i);
+            categoryButtonList.get(i).setPreferredSize(new Dimension(80, 80));
+            categoryButtonList.get(i).setFocusable(false);
         }
     }
 
