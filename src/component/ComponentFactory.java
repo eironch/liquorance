88d636f2dc98a44be20f393a11045a93de0ddfbe
ficuno <eldritchgallows@gmail.com>
2024-01-16 20:47:40 +0800

    add quantity view components

--- src/component/ComponentFactory.java
+++ src/component/ComponentFactory.java
@@ -1,5 +1,6 @@
 package component;
 
+import asset.AssetFactory;
 import main.Main;
 
 import javax.swing.*;
@@ -7,6 +8,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class ComponentFactory {
+    AssetFactory a = new AssetFactory();
     public JLabel titleText = new JLabel();
 
     // ------------- menu view --------------
@@ -16,6 +18,17 @@ public class ComponentFactory {
     public ArrayList<JButton> liquorMenuButtonList = new ArrayList<>();
     public ArrayList<JLabel> liquorMenuNameList = new ArrayList<>();
     public ArrayList<JButton> categoryButtonList = new ArrayList<>();
+
+    // ---------- quantity view -------------
+
+    public JLabel liquorBackgroundName = new JLabel();
+    public JLabel liquorImage = new JLabel();
+    public JLabel liquorForegroundName = new JLabel();
+    public JLabel liquorFlavorText = new JLabel();
+    public JLabel liquorPriceText = new JLabel();
+    public JLabel liquorCategoryText = new JLabel();
+    public JLabel liquorDescriptionText = new JLabel();
+    public JLabel liquorIngredientsText = new JLabel();
 
     // ----------- confirm view -------------
 
@@ -36,7 +49,6 @@ public class ComponentFactory {
 
     // ---------- quantity prompt -----------
 
-    public JLabel orderQuantityContext = new JLabel();
     public JButton decreaseQuantityButton = new JButton();
     public JLabel orderQuantityText = new JLabel();
     public JButton increaseQuantityButton = new JButton();
@@ -49,7 +61,7 @@ public class ComponentFactory {
 
     public ComponentFactory() {
         titleText.setText("Liquorance");
-        titleText.setFont(toHelvetica(40));
+        titleText.setFont(a.metanoia.deriveFont(55f));
         titleText.setForeground(Color.WHITE);
     }
 
@@ -78,8 +90,10 @@ public class ComponentFactory {
 
         for (int i = 0; i < 8; i++) {
             liquorMenuButtonList.add(new JButton());
-            liquorMenuButtonList.get(i).setPreferredSize(new Dimension(320, 260));
+            liquorMenuButtonList.get(i).setPreferredSize(new Dimension(260, 260));
+            liquorMenuButtonList.get(i).setBackground(Color.LIGHT_GRAY);
             liquorMenuButtonList.get(i).setFocusable(false);
+            liquorMenuButtonList.get(i).setBorder(null);
         }
 
         for (int i = 0; i < 8; i++) {
@@ -95,10 +109,54 @@ public class ComponentFactory {
             categoryButtonList.get(i).setPreferredSize(new Dimension(80, 80));
             categoryButtonList.get(i).setFocusable(false);
         }
+    }
 
-        orderQuantityContext.setText("Order Quantity");
-        orderQuantityContext.setFont(toHelvetica(30));
-        orderQuantityContext.setForeground(Color.WHITE);
+    public void handleLiquorView() {
+        changeViewButton.setText("View");
+        changeViewButton.setPreferredSize(new Dimension(100,60));
+        changeViewButton.setFocusable(false);
+
+        orderButton.setText("Order");
+        orderButton.setPreferredSize(new Dimension(200,60));
+        orderButton.setFocusable(false);
+
+        liquorBackgroundName.setForeground(Color.BLACK);
+
+        liquorImage.setPreferredSize(new Dimension(300, Main.HEIGHT - 160));
+        liquorImage.setBorder(BorderFactory.createEmptyBorder(-150, 0, 0, 0));
+        liquorImage.setHorizontalAlignment(JLabel.CENTER);
+        liquorImage.setForeground(Color.DARK_GRAY);
+
+        liquorForegroundName.setFont(a.lora.deriveFont(Font.BOLD, 24f));
+        liquorForegroundName.setHorizontalAlignment(JLabel.RIGHT);
+        liquorForegroundName.setVerticalAlignment(JLabel.BOTTOM);
+
+        liquorFlavorText.setFont(a.lora.deriveFont(17f));
+        liquorFlavorText.setPreferredSize(new Dimension(450, 120));
+        liquorFlavorText.setHorizontalAlignment(JLabel.RIGHT);
+        liquorFlavorText.setVerticalAlignment(JLabel.TOP);
+
+        liquorPriceText.setFont(a.lora.deriveFont(100f));
+        liquorPriceText.setHorizontalAlignment(JLabel.RIGHT);
+        liquorPriceText.setVerticalAlignment(JLabel.TOP);
+
+        liquorCategoryText.setFont(a.lora.deriveFont(Font.BOLD, 24f));
+        liquorCategoryText.setHorizontalAlignment(JLabel.LEFT);
+        liquorCategoryText.setVerticalAlignment(JLabel.BOTTOM);
+
+        liquorDescriptionText.setFont(a.lora.deriveFont(17f));
+        liquorDescriptionText.setPreferredSize(new Dimension(450, 120));
+        liquorDescriptionText.setHorizontalAlignment(JLabel.LEFT);
+        liquorDescriptionText.setVerticalAlignment(JLabel.TOP);
+
+        liquorIngredientsText.setFont(a.lora.deriveFont(17f));
+        liquorIngredientsText.setPreferredSize(new Dimension(450, 120));
+        liquorIngredientsText.setHorizontalAlignment(JLabel.LEFT);
+        liquorIngredientsText.setVerticalAlignment(JLabel.TOP);
+
+        cancelButton.setText("Cancel");
+        cancelButton.setPreferredSize(new Dimension(120,60));
+        cancelButton.setFocusable(false);
 
         decreaseQuantityButton.setText("-");
         decreaseQuantityButton.setPreferredSize(new Dimension(60, 60));
@@ -113,10 +171,6 @@ public class ComponentFactory {
         increaseQuantityButton.setText("+");
         increaseQuantityButton.setPreferredSize(new Dimension(60, 60));
         increaseQuantityButton.setFocusable(false);
-
-        cancelButton.setText("Cancel");
-        cancelButton.setPreferredSize(new Dimension(120,60));
-        cancelButton.setFocusable(false);
 
         confirmButton.setText("Confirm");
         confirmButton.setPreferredSize(new Dimension(120,60));
