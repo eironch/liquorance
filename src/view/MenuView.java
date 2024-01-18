eb3a788ab9c16cd76d510b83d84d4939801ca956
ficuno <eldritchgallows@gmail.com>
2024-01-18 19:07:14 +0800

    add mouse listener

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -8,20 +8,17 @@ import menu.MenuDepot;
 import panel.PanelFactory;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.*;
 
 public class MenuView {
     JFrame f;
-    DatabaseManager d = new DatabaseManager();
     PanelFactory p = new PanelFactory();
     ComponentFactory c = new ComponentFactory();
     AssetFactory a = new AssetFactory();
     MenuDepot m = new MenuDepot();
     public JPanel contentPanel = new JPanel();
-    Category currentCategory = Category.BEER;
-    int menuID;
-    int orderQuantity;
     LinkedList<LinkedList<Object>> liquorMenuList = new LinkedList<>();
     LinkedList<LinkedList<Object>> orderList = new LinkedList<>();
 
@@ -34,10 +31,11 @@ public class MenuView {
 
         contentPanel.setLayout(new BorderLayout(0, 0));
 
-        c.orderButton.addActionListener(this::confirmOrder);
+        c.returnButton.addMouseListener(createUIMouseListener(orderList, c, a));
+        c.orderButton.addMouseListener(createUIMouseListener(orderList, c, a));
 
-        p.headerContainerList.get(0).add(c.changeViewButton);
-        p.headerContainerList.get(1).add(c.titleText);
+        p.headerContainerList.get(0).add(c.returnButton);
+        p.headerContainerList.get(1).add(c.logoText);
         p.headerContainerList.get(2).add(c.orderButton);
 
         for (int i = 0; i < p.headerContainerList.size(); i++) {
@@ -45,36 +43,39 @@ public class MenuView {
         }
 
         handleMenuViewButton();
-        updateContent();
 
-        for (int i = 0; i < c.liquorMenuButtonList.size(); i++) {
-            c.liquorMenuButtonList.get(i).addActionListener(this::showQuantityPrompt);
+        for (int i = 0; i < c.liquorImageList.size(); i++) {
+            c.liquorImageList.get(i).setIcon(
+                    a.cropTopImageIcon(
+                    a.resizeIcon(
+                    (ImageIcon) m.cocktailMenuInfoList.get(i).get(5), 250, 250
+                    ), 220));
+            c.liquorImageList.get(i).addMouseListener(createMenuMouseListener(liquorMenuList, orderList, a));
+
+            c.liquorNameList.get(i).setText((String) m.cocktailMenuInfoList.get(i).get(1));
+            c.liquorNameList.get(i).addMouseListener(createMenuMouseListener(liquorMenuList, orderList, a));
+
+            liquorMenuList.get(i).set(3, m.cocktailMenuInfoList.get(i).getFirst());
         }
 
-        p.catalogRowContainerList.get(0).add(p.liquorMenuContainerList.get(0));
-        p.catalogRowContainerList.get(0).add(p.liquorMenuContainerList.get(1));
-        p.catalogRowContainerList.get(0).add(p.liquorMenuContainerList.get(2));
-        p.catalogRowContainerList.get(0).add(p.liquorMenuContainerList.get(3));
+        p.menuRowContainerList.get(0).add(p.liquorLayeredPaneList.get(0));
+        p.menuRowContainerList.get(0).add(p.liquorLayeredPaneList.get(1));
+        p.menuRowContainerList.get(0).add(p.liquorLayeredPaneList.get(2));
+        p.menuRowContainerList.get(0).add(p.liquorLayeredPaneList.get(3));
 
-        p.catalogRowContainerList.get(1).add(p.liquorMenuContainerList.get(4));
-        p.catalogRowContainerList.get(1).add(p.liquorMenuContainerList.get(5));
-        p.catalogRowContainerList.get(1).add(p.liquorMenuContainerList.get(6));
-        p.catalogRowContainerList.get(1).add(p.liquorMenuContainerList.get(7));
+        p.menuRowContainerList.get(1).add(p.liquorLayeredPaneList.get(4));
+        p.menuRowContainerList.get(1).add(p.liquorLayeredPaneList.get(5));
+        p.menuRowContainerList.get(1).add(p.liquorLayeredPaneList.get(6));
+        p.menuRowContainerList.get(1).add(p.liquorLayeredPaneList.get(7));
 
-        p.catalogSectionContainer.add(p.catalogRowContainerList.get(0));
-        p.catalogSectionContainer.add(p.catalogRowContainerList.get(1));
-
+        p.catalogSectionContainer.add(p.menuRowContainerList.get(0));
+        p.catalogSectionContainer.add(p.menuRowContainerList.get(1));
 
         p.quantitySelectorContainer.add(c.decreaseQuantityButton);
         p.quantitySelectorContainer.add(c.orderQuantityText);
         p.quantitySelectorContainer.add(c.increaseQuantityButton);
 
         p.bodyPanel.add(p.catalogSectionContainer);
-
-        for (int i = 0; i < c.categoryButtonList.size(); i++) {
-            p.categoryContainerList.get(i).add(c.categoryButtonList.get(i));
-            c.categoryButtonList.get(i).addActionListener(this::changeCategory);
-        }
 
         for (int i = 0; i < p.categoryContainerList.size(); i++) {
             p.footerPanel.add(p.categoryContainerList.get(i));
@@ -86,59 +87,24 @@ public class MenuView {
     }
 
     private void handleMenuViewButton() {
-        for (int i = 0; i < c.liquorMenuButtonList.size(); i++) {
+        for (int i = 0; i < c.liquorImageList.size(); i++) {
             int menuID = 0;
 
-            p.liquorMenuContainerList.get(i).add(c.liquorMenuButtonList.get(i));
-            p.liquorMenuContainerList.get(i).add(c.liquorMenuNameList.get(i));
+            p.liquorButtonContainerList.get(i).add(c.liquorIndicatorList.get(i));
+
+            p.liquorImageTextContainerList.get(i).add(c.liquorImageList.get(i));
+            p.liquorImageTextContainerList.get(i).add(c.liquorNameList.get(i));
+
+            p.liquorLayeredPaneList.get(i).add(p.liquorButtonContainerList.get(i), JLayeredPane.DEFAULT_LAYER);
+            p.liquorLayeredPaneList.get(i).add(p.liquorImageTextContainerList.get(i), JLayeredPane.PALETTE_LAYER);
 
             liquorMenuList.add(new LinkedList<>(Arrays.asList(
-                    p.liquorMenuContainerList.get(i), // 0
-                    c.liquorMenuButtonList.get(i), // 1
-                    c.liquorMenuNameList.get(i), // 2
+                    c.liquorIndicatorList.get(i), // 0
+                    c.liquorImageList.get(i), // 1
+                    c.liquorNameList.get(i), // 2
                     menuID // 3
             )));
         }
-    }
-
-    private void changeCategory(ActionEvent e) {
-        Component component = (Component) e.getSource();
-
-        if(component == c.categoryButtonList.get(0)) {
-            currentCategory = Category.BEER;
-        } else if(component == c.categoryButtonList.get(1)) {
-            currentCategory = Category.COCKTAIL;
-        } else if(component == c.categoryButtonList.get(2)) {
-            currentCategory = Category.RUM;
-        } else if(component == c.categoryButtonList.get(3)) {
-            currentCategory = Category.WHISKEY;
-        } else if(component == c.categoryButtonList.get(4)) {
-            currentCategory = Category.WINE;
-        }
-
-        updateContent();
-    }
-
-    private void updateContent() {
-        String categoryName;
-
-        switch (currentCategory) {
-            case BEER -> categoryName = "Beer";
-            case COCKTAIL -> categoryName = "Cocktail";
-            case RUM -> categoryName = "Rum";
-            case WHISKEY -> categoryName = "Whiskey";
-            case WINE -> categoryName = "Wine";
-            default -> categoryName = "";
-        }
-
-        for (int i = 0; i < c.liquorMenuButtonList.size(); i++) {
-            c.liquorMenuButtonList.get(i).setIcon(a.resizeIcon(
-                    (ImageIcon) m.cocktailMenuInfoList.get(i).get(5), 250, 250));
-            c.liquorMenuNameList.get(i).setText((String) m.cocktailMenuInfoList.get(i).get(1));
-            liquorMenuList.get(i).set(3, m.cocktailMenuInfoList.get(i).getFirst());
-        }
-
-        repaint(contentPanel);
     }
 
     public void updateOrder(LinkedList<LinkedList<Object>> orderInfoList) {
@@ -185,45 +151,126 @@ public class MenuView {
         removeOrderList.clear();
     }
 
-    private void showQuantityPrompt(ActionEvent e) {
-        // get menu id
-        for (LinkedList<Object> objects : liquorMenuList) {
-            if (!objects.contains(e.getSource())) {
-                continue;
-            }
-
-            menuID = (Integer) objects.getLast();
-
-            break;
-        }
-
-        Main.showLiquorView(orderList, menuID);
-    }
-
-    private void confirmOrder (ActionEvent e) {
-        if (orderList.isEmpty()) {
-            return;
-        }
-
-        Main.showConfirmView(orderList);
-    }
-
     public void clearLists() {
         orderList.clear();
     }
 
-    private void repaint(Component component){
-        SwingUtilities.invokeLater(() -> {
-            component.revalidate();
-            component.repaint();
-        });
+    private static MouseListener createUIMouseListener(LinkedList<LinkedList<Object>> orderList, ComponentFactory c, AssetFactory a) {
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JLabel button = (JLabel) e.getSource();
+
+                if (button == c.returnButton) {
+                    Main.showTitleView();
+
+                } else if (button == c.orderButton) {
+                    if (orderList.isEmpty()) {
+                        return;
+                    }
+
+                    Main.showConfirmView(orderList);
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                JLabel button = (JLabel) e.getSource();
+
+                highlightButton(button, a.burgundy, a.uiButtonSelectedIconList);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                JLabel button = (JLabel) e.getSource();
+
+                highlightButton(button, a.cocoa, a.uiButtonIconList);
+            }
+
+            public void highlightButton(JLabel button, Color color, LinkedList<ImageIcon> iconList) {
+                if (button == c.returnButton) {
+                    button.setIcon(a.resizeIcon(iconList.get(0), 200, 60));
+                    button.setForeground(color);
+                } else if (button == c.orderButton) {
+                    button.setIcon(a.resizeIcon(iconList.get(1), 200, 60));
+                    button.setForeground(color);
+                }
+            }
+        };
     }
 
-    enum Category {
-        BEER,
-        COCKTAIL,
-        RUM,
-        WHISKEY,
-        WINE
+    private static MouseListener createMenuMouseListener(LinkedList<LinkedList<Object>> liquorMenuList, LinkedList<LinkedList<Object>> orderList, AssetFactory a) {
+
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int menuID = 0;
+
+                // get menu id
+                for (LinkedList<Object> objects : liquorMenuList) {
+                    if (!objects.contains(e.getSource())) {
+                        continue;
+                    }
+
+                    menuID = (Integer) objects.getLast();
+
+                    break;
+                }
+
+                Main.showLiquorView(orderList, menuID);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                Component component = (Component) e.getSource();
+
+                highlightButton(component, a.menuButtonSelectedIcon, a.burgundy);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                Component component = (Component) e.getSource();
+
+                highlightButton(component, a.menuButtonIcon, a.cocoa);
+            }
+
+            public void highlightButton(Component component, ImageIcon imageIcon, Color color) {
+                for (LinkedList<Object> liquor : liquorMenuList) {
+                    if (!liquor.contains(component)) {
+                        continue;
+                    }
+
+                    JLabel indicator = (JLabel) liquor.getFirst();
+
+                    indicator.setIcon(a.resizeIcon(imageIcon, 280, 280));
+
+                    JLabel name =  (JLabel) liquor.get(2);
+
+                    name.setForeground(color);
+
+                    break;
+                }
+            }
+        };
     }
 }
