6f2fad7a9359f503e0a10ef8b31c7d6f0fc7ca1e
ficuno <eldritchgallows@gmail.com>
2024-01-18 22:10:31 +0800

    update code

--- src/view/ConfirmView.java
+++ src/view/ConfirmView.java
@@ -1,14 +1,12 @@
 package view;
 
+import main.Main;
 import asset.AssetFactory;
 import component.ComponentFactory;
-import database.DatabaseManager;
-import main.Main;
 import menu.MenuDepot;
 import panel.PanelFactory;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Arrays;
@@ -16,12 +14,11 @@ import java.util.LinkedList;
 
 public class ConfirmView {
     JFrame f;
-    DatabaseManager d = new DatabaseManager();
     PanelFactory p = new PanelFactory();
     ComponentFactory c = new ComponentFactory();
     AssetFactory a = new AssetFactory();
     MenuDepot m = new MenuDepot();
-    int orderContainerSize = 0;
+    static int orderContainerSize = 0;
     static int orderTotal = 0;
     public JPanel contentPanel = new JPanel();
     static LinkedList<LinkedList<Object>> orderInfoList = new LinkedList<>();
@@ -47,7 +44,7 @@ public class ConfirmView {
         c.confirmOrderButton.addMouseListener(createUIMouseListener(c, a));
 
         p.orderTotalContainerList.get(0).add(c.orderTotalContext);
-//        p.orderTotalContainerList.get(1).add();
+        p.orderTotalContainerList.get(1).add(c.decorImage);
         p.orderTotalContainerList.get(2).add(c.orderTotalText);
 
         for (int i = 0; i < p.orderTotalContainerList.size(); i++) {
@@ -87,38 +84,50 @@ public class ConfirmView {
             }
         }
 
+
         repaint(p.orderScrollPane);
     }
 
-    public void addToShownOrder(String liquorName, int orderQuantity, int price, ImageIcon liquorImage, int menuID) {
-        Container liquorOrderSectionContainer = new Container();
+    public void addToShownOrder(String liquorName, int orderQuantity, int price, ImageIcon liquorIcon, int menuID) {
+        JLayeredPane orderLayeredPane = new JLayeredPane();
+        Container orderSectionIconContainer = new Container();
+        Container orderSectionContainer = new Container();
         Container removeOrderContainer = new Container();
-        Container liquorOrderImageContainer = new Container();
-        Container liquorOrderNameContainer = new Container();
+        Container orderImageContainer = new Container();
+        Container orderNameContainer = new Container();
         Container priceQuantityContainer = new Container();
         Container quantityOrderContainer = new Container();
         Container quantitySelectorContainer = new Container();
 
-        JButton removeOrderButton = new JButton();
-        JLabel liquorOrderImage = new JLabel();
-        JLabel liquorOrderName = new JLabel();
+        JLabel orderContainerImage = new JLabel();
+        JLabel removeOrderButton = new JLabel();
+        JLabel liquorImage = new JLabel();
+        JLabel liquorNameText = new JLabel();
         JLabel liquorPriceText = new JLabel();
-        JButton decreaseQuantityButton = new JButton();
+        JLabel decreaseQuantityButton = new JLabel();
         JLabel orderQuantityText = new JLabel();
-        JButton increaseQuantityButton = new JButton();
+        JLabel increaseQuantityButton = new JLabel();
         JLabel liquorTotalText = new JLabel();
 
-        liquorOrderSectionContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
-        liquorOrderSectionContainer.setPreferredSize(new Dimension(Main.WIDTH, 80));
+        orderLayeredPane.setLayout(null);
+        orderLayeredPane.setPreferredSize(new Dimension(Main.WIDTH - 220, 80));
+
+        orderSectionIconContainer.setLayout(new GridLayout(1, 1, 0, 0));
+        orderSectionIconContainer.setSize(Main.WIDTH - 220, 80);
+
+        orderContainerImage.setIcon(a.resizeIcon(a.orderContainerIcon, Main.WIDTH - 220, 80));
+
+        orderSectionContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        orderSectionContainer.setSize(Main.WIDTH - 220, 80);
 
         removeOrderContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         removeOrderContainer.setPreferredSize(new Dimension(80, 80));
 
-        liquorOrderImageContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
-        liquorOrderImageContainer.setPreferredSize(new Dimension(80, 80));
+        orderImageContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        orderImageContainer.setPreferredSize(new Dimension(80, 80));
 
-        liquorOrderNameContainer.setLayout(new FlowLayout(FlowLayout.LEADING, 10, 20));
-        liquorOrderNameContainer.setPreferredSize(new Dimension(300, 80));
+        orderNameContainer.setLayout(new FlowLayout(FlowLayout.LEADING, 10, 22));
+        orderNameContainer.setPreferredSize(new Dimension(300, 80));
 
         priceQuantityContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         priceQuantityContainer.setPreferredSize(new Dimension(560, 80));
@@ -129,17 +138,17 @@ public class ConfirmView {
         quantitySelectorContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         quantitySelectorContainer.setPreferredSize(new Dimension(220, 60));
 
-        removeOrderButton.setText("X");
+        removeOrderButton.setIcon(a.resizeIcon(a.removeButtonIcon, 60, 60));
         removeOrderButton.setPreferredSize(new Dimension(60, 60));
         removeOrderButton.setFocusable(false);
-        removeOrderButton.addActionListener(this::removeOrder);
+        removeOrderButton.addMouseListener(createQuantityMouseListener(p, c, a));
 
-        liquorOrderImage.setIcon(a.resizeIcon(liquorImage, 75, 75));
-        liquorOrderImage.setPreferredSize(new Dimension(80, 80));
+        liquorImage.setIcon(a.resizeIcon(liquorIcon, 75, 75));
+        liquorImage.setPreferredSize(new Dimension(80, 80));
 
-        liquorOrderName.setText(liquorName);
-        liquorOrderName.setFont(a.lora.deriveFont(Font.BOLD, 25f));
-        liquorOrderName.setForeground(a.cocoa);
+        liquorNameText.setText(liquorName);
+        liquorNameText.setFont(a.lora.deriveFont(Font.BOLD, 25f));
+        liquorNameText.setForeground(a.cocoa);
 
         liquorPriceText.setText("PHP " + String.format("%,d", price));
         liquorPriceText.setFont(a.lora.deriveFont(Font.BOLD, 20f));
@@ -151,7 +160,7 @@ public class ConfirmView {
         decreaseQuantityButton.setIcon(a.resizeIcon(a.decreaseButtonIcon, 60, 60));
         decreaseQuantityButton.setPreferredSize(new Dimension(60, 60));
         decreaseQuantityButton.setFocusable(false);
-        decreaseQuantityButton.addMouseListener(createQuantityMouseListener(c, a));
+        decreaseQuantityButton.addMouseListener(createQuantityMouseListener(p, c, a));
 
         orderQuantityText.setText(String.valueOf(orderQuantity));
         orderQuantityText.setPreferredSize(new Dimension(100, 60));
@@ -162,7 +171,7 @@ public class ConfirmView {
         increaseQuantityButton.setIcon(a.resizeIcon(a.increaseButtonIcon, 60, 60));
         increaseQuantityButton.setPreferredSize(new Dimension(60, 60));
         increaseQuantityButton.setFocusable(false);
-        increaseQuantityButton.addMouseListener(createQuantityMouseListener(c, a));
+        increaseQuantityButton.addMouseListener(createQuantityMouseListener(p, c, a));
 
         liquorTotalText.setText("PHP " + String.format("%,d", price * orderQuantity) + ".00");
         liquorTotalText.setFont(a.lora.deriveFont(Font.BOLD,20f));
@@ -173,8 +182,8 @@ public class ConfirmView {
 
         removeOrderContainer.add(removeOrderButton);
 
-        liquorOrderImageContainer.add(liquorOrderImage);
-        liquorOrderNameContainer.add(liquorOrderName);
+        orderImageContainer.add(liquorImage);
+        orderNameContainer.add(liquorNameText);
 
         quantitySelectorContainer.add(decreaseQuantityButton);
         quantitySelectorContainer.add(orderQuantityText);
@@ -186,14 +195,14 @@ public class ConfirmView {
         priceQuantityContainer.add(quantityOrderContainer);
         priceQuantityContainer.add(liquorTotalText);
 
-        liquorOrderSectionContainer.add(removeOrderContainer);
-        liquorOrderSectionContainer.add(liquorOrderImageContainer);
-        liquorOrderSectionContainer.add(liquorOrderNameContainer);
-        liquorOrderSectionContainer.add(priceQuantityContainer);
+        orderSectionContainer.add(removeOrderContainer);
+        orderSectionContainer.add(orderImageContainer);
+        orderSectionContainer.add(orderNameContainer);
+        orderSectionContainer.add(priceQuantityContainer);
 
         orderInfoList.add(new LinkedList<>(Arrays.asList(
                 menuID, // 0
-                liquorOrderSectionContainer, // 1
+                orderLayeredPane, // 1
                 removeOrderButton, // 2
                 decreaseQuantityButton, // 3
                 orderQuantityText, // 4
@@ -213,26 +222,15 @@ public class ConfirmView {
 
         showOrderTotal();
 
-        orderContainerSize += 80;
+        orderContainerSize += 100;
+
+        orderSectionIconContainer.add(orderContainerImage);
+
+        orderLayeredPane.add(orderSectionIconContainer, JLayeredPane.DEFAULT_LAYER);
+        orderLayeredPane.add(orderSectionContainer, JLayeredPane.PALETTE_LAYER);
 
         p.orderPanel.setPreferredSize(new Dimension(Main.WIDTH, orderContainerSize));
-        p.orderPanel.add(liquorOrderSectionContainer);
-    }
-
-    private void removeOrder(ActionEvent e) {
-        for (int i = 0; i < orderInfoList.size(); i++) {
-            if (!orderInfoList.get(i).contains(e.getSource())) {
-                continue;
-            }
-
-            p.orderPanel.remove((Component) orderInfoList.get(i).get(1));
-
-            orderInfoList.remove(i);
-
-            repaint(p.orderPanel);
-
-            return;
-        }
+        p.orderPanel.add(orderLayeredPane);
     }
 
     private void showOrderTotal() {
@@ -259,7 +257,7 @@ public class ConfirmView {
         });
     }
 
-    private static MouseListener createQuantityMouseListener(ComponentFactory c, AssetFactory a) {
+    private static MouseListener createQuantityMouseListener(PanelFactory p, ComponentFactory c, AssetFactory a) {
         return new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -270,10 +268,20 @@ public class ConfirmView {
             public void mousePressed(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
-                if (button == c.decreaseQuantityButton) {
-                    decreaseQuantity(button);
-                } else if (button == c.increaseQuantityButton) {
-                    increaseQuantity(button);
+                for (LinkedList<Object> order : orderInfoList) {
+                    if (!order.contains(button)) {
+                        continue;
+                    }
+
+                    if (button == order.get(3)) {
+                        decreaseQuantity(button);
+                    } else if (button == order.get(5)) {
+                        increaseQuantity(button);
+                    } else if (button == order.get(2)) {
+                        removeOrder(button);
+                    }
+
+                    break;
                 }
             }
 
@@ -286,10 +294,20 @@ public class ConfirmView {
             public void mouseEntered(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
-                if (button == c.decreaseQuantityButton) {
-                    button.setIcon(a.resizeIcon(a.decreaseSelectedButtonIcon, 60, 60));
-                } else if (button == c.increaseQuantityButton) {
-                    button.setIcon(a.resizeIcon(a.increaseSelectedButtonIcon, 60, 60));
+                for (LinkedList<Object> order : orderInfoList) {
+                    if (!order.contains(button)) {
+                        continue;
+                    }
+
+                    if (button == order.get(3)) {
+                        button.setIcon(a.resizeIcon(a.decreaseSelectedButtonIcon, 60, 60));
+                    } else if (button == order.get(5)) {
+                        button.setIcon(a.resizeIcon(a.increaseSelectedButtonIcon, 60, 60));
+                    } else if (button == order.get(2)) {
+                        button.setIcon(a.resizeIcon(a.removeSelectedButtonIcon, 60, 60));
+                    }
+
+                    break;
                 }
             }
 
@@ -297,11 +315,42 @@ public class ConfirmView {
             public void mouseExited(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
-                if (button == c.decreaseQuantityButton) {
-                    button.setIcon(a.resizeIcon(a.decreaseButtonIcon, 60, 60));
-                } else if (button == c.increaseQuantityButton) {
-                    button.setIcon(a.resizeIcon(a.increaseButtonIcon, 60, 60));
+                for (LinkedList<Object> order : orderInfoList) {
+                    if (!order.contains(button)) {
+                        continue;
+                    }
+
+                    if (button == order.get(3)) {
+                        button.setIcon(a.resizeIcon(a.decreaseButtonIcon, 60, 60));
+                    } else if (button == order.get(5)) {
+                        button.setIcon(a.resizeIcon(a.increaseButtonIcon, 60, 60));
+                    } else if (button == order.get(2)) {
+                        button.setIcon(a.resizeIcon(a.removeButtonIcon, 60, 60));
+                    }
+
+                    break;
                 }
+            }
+
+            private void removeOrder(JLabel button) {
+                for (int i = 0; i < orderInfoList.size(); i++) {
+                    if (!orderInfoList.get(i).contains(button)) {
+                        continue;
+                    }
+
+                    p.orderPanel.remove((Component) orderInfoList.get(i).get(1));
+
+                    orderInfoList.remove(i);
+
+                    break;
+                }
+
+                showOrderTotal();
+
+                orderContainerSize -= 100;
+                p.orderPanel.setPreferredSize(new Dimension(Main.WIDTH, orderContainerSize));
+
+                repaint(p.orderPanel);
             }
 
             private void decreaseQuantity(JLabel buttonPressed) {
@@ -315,11 +364,11 @@ public class ConfirmView {
                     if (orderQuantity == 1) {
                         return;
                     } else if (--orderQuantity == 1) {
-                        JButton button = (JButton) order.get(3);
+                        JLabel button = (JLabel) order.get(3);
                         button.setEnabled(false);
                     }
 
-                    JButton button = (JButton) order.get(5);
+                    JLabel button = (JLabel) order.get(5);
 
                     if (!button.isEnabled()) {
                         button.setEnabled(true);
@@ -350,11 +399,11 @@ public class ConfirmView {
                     if (orderQuantity == 99) {
                         return;
                     } else if (++orderQuantity == 99) {
-                        JButton button = (JButton) order.get(5);
+                        JLabel button = (JLabel) order.get(5);
                         button.setEnabled(false);
                     }
 
-                    JButton button = (JButton) order.get(3);
+                    JLabel button = (JLabel) order.get(3);
 
                     if (!button.isEnabled()) {
                         button.setEnabled(true);
@@ -386,6 +435,13 @@ public class ConfirmView {
                         "PHP " + String.format("%,d", orderTotal) + ".00" +
                         "</div></html>"
                 );
+            }
+
+            private void repaint(Component component){
+                SwingUtilities.invokeLater(() -> {
+                    component.revalidate();
+                    component.repaint();
+                });
             }
         };
     }
