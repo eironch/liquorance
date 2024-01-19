2c4397afdedb09e095047838232ec52d0f6b797f
ficuno <eldritchgallows@gmail.com>
2024-01-19 18:47:25 +0800

    update code

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -18,6 +18,7 @@ public class MenuView {
     AssetFactory a = new AssetFactory();
     MenuDepot m = new MenuDepot();
     public JPanel contentPanel = new JPanel();
+    int orderQuantity = 0;
     LinkedList<LinkedList<Object>> liquorMenuList = new LinkedList<>();
     LinkedList<LinkedList<Object>> orderList = new LinkedList<>();
 
@@ -40,7 +41,24 @@ public class MenuView {
             p.headerPanel.add(p.headerContainerList.get(i));
         }
 
-        handleMenuViewButton();
+        for (int i = 0; i < c.liquorImageList.size(); i++) {
+            int menuID = 0;
+
+            p.liquorButtonContainerList.get(i).add(c.liquorIndicatorList.get(i));
+
+            p.liquorImageTextContainerList.get(i).add(c.liquorImageList.get(i));
+            p.liquorImageTextContainerList.get(i).add(c.liquorNameList.get(i));
+
+            p.liquorLayeredPaneList.get(i).add(p.liquorButtonContainerList.get(i), JLayeredPane.DEFAULT_LAYER);
+            p.liquorLayeredPaneList.get(i).add(p.liquorImageTextContainerList.get(i), JLayeredPane.PALETTE_LAYER);
+
+            liquorMenuList.add(new LinkedList<>(Arrays.asList(
+                    c.liquorIndicatorList.get(i), // 0
+                    c.liquorImageList.get(i), // 1
+                    c.liquorNameList.get(i), // 2
+                    menuID // 3
+            )));
+        }
 
         for (int i = 0; i < c.liquorImageList.size(); i++) {
             c.liquorImageList.get(i).setIcon(
@@ -77,27 +95,6 @@ public class MenuView {
 
         contentPanel.add(p.headerPanel, BorderLayout.NORTH);
         contentPanel.add(p.bodyPanel, BorderLayout.CENTER);
-    }
-
-    private void handleMenuViewButton() {
-        for (int i = 0; i < c.liquorImageList.size(); i++) {
-            int menuID = 0;
-
-            p.liquorButtonContainerList.get(i).add(c.liquorIndicatorList.get(i));
-
-            p.liquorImageTextContainerList.get(i).add(c.liquorImageList.get(i));
-            p.liquorImageTextContainerList.get(i).add(c.liquorNameList.get(i));
-
-            p.liquorLayeredPaneList.get(i).add(p.liquorButtonContainerList.get(i), JLayeredPane.DEFAULT_LAYER);
-            p.liquorLayeredPaneList.get(i).add(p.liquorImageTextContainerList.get(i), JLayeredPane.PALETTE_LAYER);
-
-            liquorMenuList.add(new LinkedList<>(Arrays.asList(
-                    c.liquorIndicatorList.get(i), // 0
-                    c.liquorImageList.get(i), // 1
-                    c.liquorNameList.get(i), // 2
-                    menuID // 3
-            )));
-        }
     }
 
     public void updateOrder(LinkedList<LinkedList<Object>> orderInfoList) {
@@ -142,22 +139,52 @@ public class MenuView {
         }
 
         removeOrderList.clear();
+
+        updateOrderQuantity();
+    }
+
+    // change total quantity beside the order text
+    public void updateOrderQuantity() {
+        orderQuantity = 0;
+
+        for (LinkedList<Object> order : orderList) {
+            orderQuantity += (int) order.getLast();
+        }
+
+        c.orderButton.setText("Order (" + orderQuantity +")");
+
+        repaint(c.orderButton);
     }
 
     public void clearLists() {
         orderList.clear();
     }
 
+    private void repaint(Component component){
+        SwingUtilities.invokeLater(() -> {
+            component.revalidate();
+            component.repaint();
+        });
+    }
+
+    // custom button functionality for the ui
     private static MouseListener createUIMouseListener(
             LinkedList<LinkedList<Object>> orderList, ComponentFactory c, AssetFactory a) {
         return new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
+                highlightButton(button, a.burgundy, a.uiButtonSelectedIconList);
+
+                // call respective method for the buttons
                 if (button == c.returnButton) {
                     Main.showTitleView();
-
                 } else if (button == c.orderButton) {
                     if (orderList.isEmpty()) {
                         return;
@@ -165,11 +192,6 @@ public class MenuView {
 
                     Main.showConfirmView(orderList);
                 }
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
             }
 
             @Override
@@ -203,6 +225,7 @@ public class MenuView {
         };
     }
 
+    // custom button functionality for the menu
     private static MouseListener createMenuMouseListener(
             LinkedList<LinkedList<Object>> liquorMenuList,
             LinkedList<LinkedList<Object>> orderList,
@@ -215,20 +238,11 @@ public class MenuView {
 
             @Override
             public void mousePressed(MouseEvent e) {
-                int menuID = 0;
+                Component component = (Component) e.getSource();
 
-                // get menu id
-                for (LinkedList<Object> objects : liquorMenuList) {
-                    if (!objects.contains(e.getSource())) {
-                        continue;
-                    }
+                highlightButton(component, a.menuButtonSelectedIcon, a.burgundy);
 
-                    menuID = (Integer) objects.getLast();
-
-                    break;
-                }
-
-                Main.showLiquorView(orderList, menuID);
+                Main.showLiquorView(orderList, getMenuID(e));
             }
 
             @Override
@@ -250,18 +264,35 @@ public class MenuView {
                 highlightButton(component, a.menuButtonIcon, a.cocoa);
             }
 
+            public int getMenuID(MouseEvent e){
+                int menuID = 0;
+
+                // get menu id
+                for (LinkedList<Object> objects : liquorMenuList) {
+                    if (!objects.contains(e.getSource())) {
+                        continue;
+                    }
+
+                    menuID = (Integer) objects.getLast();
+
+                    break;
+                }
+
+                return menuID;
+            }
+
             public void highlightButton(Component component, ImageIcon imageIcon, Color color) {
                 for (LinkedList<Object> liquor : liquorMenuList) {
                     if (!liquor.contains(component)) {
                         continue;
                     }
 
+                    // highlight
                     JLabel indicator = (JLabel) liquor.getFirst();
-
                     indicator.setIcon(a.resizeIcon(imageIcon, 280, 280));
 
+                    // change text color
                     JLabel name =  (JLabel) liquor.get(2);
-
                     name.setForeground(color);
 
                     break;
