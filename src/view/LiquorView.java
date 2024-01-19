f2bac5205b9aa28f4e51c08ab9cb59b125140693
ficuno <eldritchgallows@gmail.com>
2024-01-19 18:47:01 +0800

    update code

--- src/view/LiquorView.java
+++ src/view/LiquorView.java
@@ -1,4 +1,4 @@
-package quantity;
+package view;
 
 import main.Main;
 import asset.AssetFactory;
@@ -20,6 +20,7 @@ public class LiquorView {
     MenuDepot m = new MenuDepot();
     static int menuID;
     static int orderQuantity;
+    static int orderTotalQuantity;
     public JPanel contentPanel = new JPanel();
     static LinkedList<LinkedList<Object>> orderList = new LinkedList<>();
 
@@ -31,7 +32,6 @@ public class LiquorView {
 
         contentPanel.setLayout(new BorderLayout(0, 0));
         contentPanel.setBackground(a.burgundy);
-
 
         c.orderButton.addMouseListener(createUIMouseListener(m.cocktailMenuInfoList, c, a));
         c.returnButton.addMouseListener(createUIMouseListener(m.cocktailMenuInfoList, c, a));
@@ -96,6 +96,7 @@ public class LiquorView {
         contentPanel.add(p.footerPanel, BorderLayout.SOUTH);
     }
 
+    // shows the components to the specified liquor
     public void showLiquor(LinkedList<LinkedList<Object>> orderList, int menuID) {
         LiquorView.orderList = orderList;
         LiquorView.menuID = menuID;
@@ -131,26 +132,38 @@ public class LiquorView {
             c.liquorForegroundName.setText(String.valueOf(liquor.get(1)));
 
             c.liquorFlavorText.setText("<html><div style='text-align: right;'>" +
+                    "<font style='font-family:" +
+                    a.lora.getFamily() +
+                    "; font-size:16pt; font-weight:normal;'>" +
                     liquor.get(2) +
-                    "</div></html>"
+                    "</font></div></html>"
             );
 
             c.liquorPriceText.setText("PHP " + liquor.getLast());
 
-            c.liquorCategoryText.setText("<html><i>" +
+            c.liquorCategoryText.setText("<html><div style='text-align: left;'>" +
+                    "<font style='font-family:" +
+                    a.lora.getFamily() +
+                    "; font-size:24pt; font-weight:normal;'><i>" +
                     "Cocktail" +
-                    "</i></html>"
+                    "</i></font></html>"
             );
 
-            c.liquorDescriptionText.setText("<html><div style='text-align: left;'><i>" +
+            c.liquorDescriptionText.setText("<html><div style='text-align: left;'>" +
+                    "<font style='font-family:" +
+                    a.lora.getFamily() +
+                    "; font-size:17pt; font-weight:normal;'><i>" +
                     liquor.get(3) +
-                    "</i></div></html>"
+                    "</i></font></div></html>"
             );
 
-            c.liquorIngredientsText.setText("<html><div style='text-align: left;'><i>" +
+            c.liquorIngredientsText.setText("<html><div style='text-align: left;'>" +
+                    "<font style='font-family:" +
+                    a.lora.getFamily() +
+                    "; font-size:17pt; font-weight:normal;'><i>" +
                     "Ingredients:<br>" +
                     liquor.get(4) +
-                    "</i></div></html>"
+                    "</i></font></div></html>"
             );
 
             repaint(p.liquorLayeredPane);
@@ -158,8 +171,21 @@ public class LiquorView {
             break;
         }
 
+        updateOrderQuantity();
+
         repaint(p.liquorLayeredPane);
     }
+
+    public void updateOrderQuantity() {
+        orderTotalQuantity = 0;
+
+        for (LinkedList<Object> order : orderList) {
+            orderTotalQuantity += (int) order.getLast();
+        }
+
+        c.orderButton.setText("Order (" + orderTotalQuantity +")");
+    }
+
 
     public void clearLists() {
         orderList.clear();
@@ -172,6 +198,7 @@ public class LiquorView {
         });
     }
 
+    // custom button functionality for the quantity selector
     private static MouseListener createQuantityMouseListener(ComponentFactory c, AssetFactory a) {
         return new MouseListener() {
             @Override
@@ -183,6 +210,14 @@ public class LiquorView {
             public void mousePressed(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
+                // highlight button
+                if (button == c.decreaseQuantityButton) {
+                    button.setIcon(a.resizeIcon(a.decreaseSelectedButtonIcon, 60, 60));
+                } else if (button == c.increaseQuantityButton) {
+                    button.setIcon(a.resizeIcon(a.increaseSelectedButtonIcon, 60, 60));
+                }
+
+                // call respective methods for the buttons
                 if (button == c.decreaseQuantityButton) {
                     decreaseQuantity();
                 } else if (button == c.increaseQuantityButton) {
@@ -199,6 +234,7 @@ public class LiquorView {
             public void mouseEntered(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
+                // highlight button
                 if (button == c.decreaseQuantityButton) {
                     button.setIcon(a.resizeIcon(a.decreaseSelectedButtonIcon, 60, 60));
                 } else if (button == c.increaseQuantityButton) {
@@ -210,6 +246,7 @@ public class LiquorView {
             public void mouseExited(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
+                // remove highlight on button
                 if (button == c.decreaseQuantityButton) {
                     button.setIcon(a.resizeIcon(a.decreaseButtonIcon, 60, 60));
                 } else if (button == c.increaseQuantityButton) {
@@ -239,6 +276,7 @@ public class LiquorView {
         };
     }
 
+    // custom button functionality for the ui
     private static MouseListener createUIMouseListener(
             LinkedList<LinkedList<Object>> cocktailMenuInfoList,
             ComponentFactory c, AssetFactory a) {
@@ -252,6 +290,10 @@ public class LiquorView {
             public void mousePressed(MouseEvent e) {
                 JLabel button = (JLabel) e.getSource();
 
+                highlightButton(button, a.burgundy, a.uiButtonSelectedIconList);
+
+
+                // call respective method for the buttons
                 if (button == c.returnButton) {
                     Main.showTitleView();
                 } else if (button == c.orderButton) {
